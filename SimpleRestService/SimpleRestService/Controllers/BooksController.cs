5282a12f0638af8fec99cbad219776a6e97803b1
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2021-10-10 12:10:25 +0200

    simple REST service
    Assignment 2

---
+++ SimpleRestService/SimpleRestService/Controllers/BooksController.cs
@@ -0,0 +1,55 @@
+﻿using ClassLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleRestService.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRestService.Controllers
+{
+    [Route("api/[controller]")]
+    // the controller is available on ..../api/books
+    // [controller] means the name of the controller minus "Controller"
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksManager _manager = new BooksManager();
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{ISBN13}")]
+        public Book Get(string ISBN13)
+        {
+            return _manager.GetById(ISBN13);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public Book Post([FromBody] string title, string author, int pageNumber)
+        {
+            return _manager.Create(title, author, pageNumber);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{ISBN13}")]
+        public Book Put([FromBody] Book value)
+        {
+            return _manager.Update(value);
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{ISBN13}")]
+        public Book Delete(string ISBN13)
+        {
+            return _manager.Delete(ISBN13);
+        }
+    }
+}
---
+++ SimpleRestService/SimpleRestService/Managers/BooksManager.cs
@@ -0,0 +1,60 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRestService.Managers
+{
+    public class BooksManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<Book> Data = new List<Book>()        
+        {
+            new Book {ISBN13 = "ISBN13" + _nextId++ , Title = "C# is nice", Author = "Morten Myfriend", PageNumber = 159 },
+            new Book {ISBN13 = "ISBN13" + _nextId++ , Title = "JavaScript is nice", Author = "Martin Myfriend", PageNumber = 180 },
+            new Book {ISBN13 = "ISBN13" + _nextId++ , Title = "ASP.Net is nice", Author = "Mohammed Myfriend", PageNumber = 175 },
+            
+        };
+           
+        public List<Book> GetAll()
+        {            
+            //return new List<Book>(Data);
+            return Data;
+            // copy constructor
+            // Callers should no get a reference to the Data object, but rather get a copy
+        }
+
+        public Book GetById(string ISBN)
+        {
+            return Data.Find(book => book.ISBN13 == ISBN);
+        }
+
+        public Book Create(string title, string author, int pageNumber)
+        {
+            Book newBook = new Book();
+            newBook.ISBN13 = "ISBN13" + _nextId++;
+            Data.Add(newBook);
+            return newBook;
+        }
+
+        public Book Delete(string ISBN)
+        {
+            Book book = Data.Find(book => book.ISBN13 == ISBN);
+            if (book == null) 
+                return null;
+            Data.Remove(book);
+            return book;
+        }
+
+        public Book Update(Book bookToUpdate)
+        {
+            Book book = Data.Find(book => book.ISBN13 == bookToUpdate.ISBN13);
+            if (book == null) return null;
+            book.Title = bookToUpdate.Title;
+            book.Author = bookToUpdate.Author;
+            book.PageNumber = bookToUpdate.PageNumber;
+            return book;
+        }
+    }
+}
